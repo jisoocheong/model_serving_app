ef537469bd68d3c652fa20eabbd6f8db23770625
jcheong <jisoocheong@gmail.com>
2021-07-29 16:11:28 +0900

    progress on page for creating new user

--- frontend/src/Components/CreateAccount.jsx
+++ frontend/src/Components/CreateAccount.jsx
@@ -1,8 +1,42 @@
 import React, {useState, useEffect} from "react";
 import { render } from 'react-dom';
 import { Redirect } from "react-router-dom";
-import { TextField } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
     
+
+var inputUsername = "";
+var inputFirstPassword = "";
+var inputSecondPassword = "";
+
+function NewUser(){
+    const handleSubmit = () => {
+        var isValidUser = false;
+        const responseResult = fetch("http://127.0.0.1:8000/create", {
+            
+        // fill in request
+
+
+        }).then(response = > {
+            return response.json();
+        }).then(data => {
+            // fill in with valid new user info
+
+            if (isValidUser) {
+                history.push()
+            }
+        })
+
+
+
+    }
+
+
+    return (
+        <Button variant="contained" color="primary" onClick={handleSubmit}>Sign Up</Button>
+    
+    )
+}
+
 
 export default function CreateAccount() {
     const [search, setSearch] = useState([])
@@ -11,59 +45,45 @@ export default function CreateAccount() {
         const search = await response.json()
         setSearch(search.data)
     }
+
     useEffect(() => {
         fetchSearch()
     }, [])
-    return (
-        <div>
-                <h3>Search for a model</h3>
-                <label htmlFor="model">Model:</label><br/>
-                <TextField id="search" size="small" variant="outlined"/>
-        </div>
-    
-    );
-}
 
-
-
-export default function Login() {
-    const [login, setLogin] = useState([]);
-    const fetchLogin = async () => {
-        const response = await fetch("http://127.0.0.1:8000/");
-        const login_input = await response.json();
-        setLogin(login_input.data);
-    };
-    useEffect(() => {
-        fetchLogin();
-    }, []);
 
 
 
     const setUsername = event => {
         inputUsername = event.target.value
     };
-
-    const setPassword = event => {
-        inputPassword = event.target.value
+    const setFirstPassword = event => {
+        inputFirstPassword = event.target.value
     };
+    const setSecondPassword = event => {
+        inputSecondPassword = event.target.value
+    };
+
 
     return (
         <div>
-            <h3>Sign In</h3>
-            <div className="form-group">
+            <h3>Sign Up</h3>
+           <div className="form-group">
                 <label>Username</label><br />
                 <TextField id="username" size="small" label="Enter username" variant="outlined" onChange={setUsername} />
             </div>
 
             <div className="form-group">
                 <label>Password</label> <br />
-                <TextField id="password" type="password" size="small" label="Enter password" variant="outlined" onChange={setPassword} />
+                <TextField id="password" type="password" size="small" label="Enter password" variant="outlined" onChange={setFirstPassword} />
             </div>
-            <SubmitLogin/>
+            <div className="form-group">
+                <label>Reenter Password</label> <br />
+                <TextField id="password" type="password" size="small" label="Enter password" variant="outlined" onChange={setSecondPassword} />
+            </div>
+            <NewUser/>
         </div>
 
     )
-
 
 
 };
