19fe5d1bb87fa2d3344916d8b3ae4c44c3164024
mookyung song <toilety@gmail.com>
2021-08-04 14:18:32 +0900

    proper expiration time setting

--- backend/app/security.py
+++ backend/app/security.py
@@ -30,7 +30,7 @@ def create_access_token(
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minute)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
     return encoded_jwt
