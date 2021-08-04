6d5fdf9cb7cc4af34eca7126c32978353079df8a
jcheong <jisoocheong@gmail.com>
2021-08-04 10:11:57 +0900

    model_table properly takes in screenshots

--- backend/app/model_serving_db/model_table.py
+++ backend/app/model_serving_db/model_table.py
@@ -5,6 +5,7 @@ from creators import *
 def add_model(username: str, framework: str, name: str, version: list, size: str, device_dep: list, description: str, tags: list, input: str, output: str, test_code: str, screenshot: list):
     """
     Adds the given model to the model_table. 
+    Note that the screenshots will be a list of the path to file of the image
     Returns true if the model is successfully added, false otherwise
     """
     # Creates db and table if those don't already exist
@@ -24,16 +25,34 @@ def add_model(username: str, framework: str, name: str, version: list, size: str
     highest_id = cursor.fetchone()
     highest_id = 0 if highest_id is None else int(highest_id[0])
 
+
+    # Get blobs of screenshots
+    blobs_str = "["
+    for img in screenshot:
+        try:
+            drawing = open(img, 'rb').read()
+            blob = psycopg2.Binary(drawing)
+            blobs_str = blobs_str + f"{blob},"
+        except Exception as error:
+            return result 
+    
+    blobs_str = blobs_str[:-1] + "]"
+
     cursor.execute(f'''SELECT * FROM model_table WHERE name = '{name}';''')
     existing_model = cursor.fetchone()
     if existing_model is None:
         result = True
-        cursor.execute(f'''INSERT INTO model_table(id, adder_username, framework, name, version, size, device_dependency, description, tags, input, output, test_code, screenshot) VALUES ({highest_id + 1}, '{username}', '{framework}', '{name}', ARRAY{version}, '{size}', ARRAY{device_dep}, '{description}', ARRAY{tags}, '{input}', '{output}', '{test_code}', ARRAY[{psycopg2.Binary(b'test')}]);''')
+        cursor.execute('''INSERT INTO model_table(id, adder_username, framework, name,''' + \
+                ''' version, size, device_dependency, description, tags, input, output, test_code, screenshot) ''' + \
+                        f'''VALUES ({highest_id + 1}, '{username}', '{framework}', '{name}', ARRAY{version}, '{size}' ''' + \
+                        f''', ARRAY{device_dep}, '{description}', ARRAY{tags}, '{input}', '{output}', ''' + \
+                        f''''{test_code}', ARRAY{blobs_str});''')
          
         print("Successfully added a new model")
 
     # Close the connection
     conn.close()
+    return result
 
 
 
--- backend/app/model_serving_db/test.py
+++ backend/app/model_serving_db/test.py
@@ -3,7 +3,7 @@ from model_table import *
 import psycopg2
 
 
-print(add_model("jisoo", "tensorflow", "stylegan2", ["anime", "toonify"], "various sizes", ['GPU'], "description", ["image generation", "face"], "latents:[None, 512] float32", "Base64 encoded RGB images: [None,] String", "some test code", ["image content"])) 
+print(add_model("jisoo", "tensorflow", "stylegan2", ["anime", "toonify"], "various sizes", ['GPU'], "description", ["image generation", "face"], "latents:[None, 512] float32", "Base64 encoded RGB images: [None,] String", "some test code", ["cat.jpeg", "pikachu.png"])) 
     
 
 
